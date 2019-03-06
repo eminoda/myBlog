cacc024dea1e5bd05d3f4f975a58a030e4612902
sxh@9niu.com <245978782@qq.com>
2019-03-06 18:34:23 +0800

    update #

--- read_note/js/demo/clazz.js
+++ read_note/js/demo/clazz.js
@@ -1,3 +1,4 @@
+// es6 class 方式继承
 class Parent {
 	constructor() {
 		this.name = 'Parent';
@@ -16,6 +17,7 @@ class Child extends Parent {
 const instance = new Child();
 console.log(instance);
 
+// es5 旧方式定义继承
 function ParentES5() {
 	this.name = 'Parent';
 }
@@ -32,3 +34,21 @@ ChildES5.prototype = prototype;
 
 var instanceES5 = new ChildES5();
 console.log(instanceES5);
+
+// 对象 getter/setter 在 class 绑定一些技巧
+var obj = {
+	name: 123
+};
+class TestProperDefined {
+	constructor() {
+		Object.defineProperty(obj, 'service', {
+			get() {
+				console.log(this); // { name: 123 }
+				return 'service';
+			}
+		});
+	}
+}
+new TestProperDefined();
+
+console.log(obj.service); // service

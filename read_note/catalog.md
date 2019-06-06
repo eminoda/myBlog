4df08035bd9ceb49b20989ee2985a2c9c9dd97a1
sxh@9niu.com <245978782@qq.com>
2019-06-06 11:26:05 +0800

    update #path

--- read_note/catalog.md
+++ read_note/catalog.md
@@ -2,21 +2,25 @@
 
 ## [前言](https://github.com/eminoda/myBlog/issues/1)
 
-## js
+## Javascript
 
--   [js 基础--面向对象 1.描述对象属性的属性特征](https://github.com/eminoda/myBlog/issues/2)
--   [js 基础--面向对象 2.对象创建](https://github.com/eminoda/myBlog/issues/3)
--   [js 基础--面向对象 3.原型那些事 ](https://github.com/eminoda/myBlog/issues/4)
--   [js 基础--面向对象 4.几种继承方式](https://github.com/eminoda/myBlog/issues/5)
--   [js 基础--函数表达式 闭包](https://github.com/eminoda/myBlog/issues/6)
--   [js 基础--数组 常用方法](https://github.com/eminoda/myBlog/issues/9)
--   [js 基础--正则表达式](https://github.com/eminoda/myBlog/issues/10)
--   [js 基础--this](https://github.com/eminoda/myBlog/issues/16)
--   [js 扩展--defineProperty 使用场景](https://github.com/eminoda/myBlog/issues/13)
--   [js 扩展--bind call apply 那些事](https://github.com/eminoda/myBlog/issues/14)
--   [js es6--proxy](https://github.com/eminoda/myBlog/issues/11)
+-   [js 基础 -- 面向对象 1.描述对象属性的属性特征](https://github.com/eminoda/myBlog/issues/2)
+-   [js 基础 -- 面向对象 2.对象创建](https://github.com/eminoda/myBlog/issues/3)
+-   [js 基础 -- 面向对象 3.原型那些事 ](https://github.com/eminoda/myBlog/issues/4)
+-   [js 基础 -- 面向对象 4.几种继承方式](https://github.com/eminoda/myBlog/issues/5)
+-   [js 基础 -- 函数表达式 闭包](https://github.com/eminoda/myBlog/issues/6)
+-   [js 基础 -- 数组 常用方法](https://github.com/eminoda/myBlog/issues/9)
+-   [js 基础 -- 正则表达式](https://github.com/eminoda/myBlog/issues/10)
+-   [js 基础 -- this](https://github.com/eminoda/myBlog/issues/16)
+-   [js 基础 -- cookie](https://github.com/eminoda/myBlog/issues/19)
+-   [js 扩展 -- defineProperty 使用场景](https://github.com/eminoda/myBlog/issues/13)
+-   [js 扩展 -- bind call apply 那些事](https://github.com/eminoda/myBlog/issues/14)
+-   [js 扩展 -- JSON 序列化中忽视的小方法](https://github.com/eminoda/myBlog/issues/18)
+-   [js ES6 -- proxy](https://github.com/eminoda/myBlog/issues/11)
+-   [js ES6 -- class](https://github.com/eminoda/myBlog/issues/17)
 
 ## Node.js
 
--   [node 基础--exports 和 module.exports 的区别？](https://github.com/eminoda/myBlog/issues/7)
--   [node 基础--npm install --save-dev 和 --save 的区别](https://github.com/eminoda/myBlog/issues/8)
+-   [node 基础 -- exports 和 module.exports 的区别？](https://github.com/eminoda/myBlog/issues/7)
+-   [node 基础 -- npm install --save-dev 和 --save 的区别？](https://github.com/eminoda/myBlog/issues/8)
+-   [node 基础 -- path](https://github.com/eminoda/myBlog/issues/20)
--- read_note/node/demo/path/index.js
+++ read_note/node/demo/path/index.js
@@ -1,39 +1,33 @@
 const path = require('path');
 
-// 由共同路径开始，后者绝对路径，只输出绝对路径
-// console.log(path.resolve('/test/foo/boo', '/test/a.js'));
-// e:\test\a.js
+// path.resolve
+console.log('path.resolve', path.resolve('/test/foo/boo', '/test/a.js'));
+console.log('path.resolve', path.resolve('/other/foo', '/test/a.js'));
+console.log('path.resolve', path.resolve(__dirname, 'test/a.js'));
 
-// 不相干路径，后者绝对路径，只输出绝对路径
-// console.log(path.resolve('/other/foo', '/test/a.js'));
-// e:\test\a.js
+// path.relative
+console.log('path.resolve', path.resolve('/test/foo/boo'));
+console.log('path.resolve', path.resolve('/test/a.js'));
+console.log('path.relative', path.relative('/test/foo/boo', '/test/a.js'));
+console.log('path.resolve', path.resolve('/other/foo/boo'));
+console.log('path.resolve', path.resolve('/test/a.js'));
+console.log('path.relative', path.relative('/other/foo', '/test/a.js'));
+console.log('path.relative', path.relative(__dirname, 'test/a.js'));
 
-// 相对路径
-// console.log(path.resolve(__dirname, 'test/a.js'));
-// e:\my_work\github\myBlog\read_note\node\demo\path\test\a.js
-
-
-
-// console.log(path.relative('/test/foo/boo', '/test/a.js'));
-// console.log(path.resolve('/test/foo/boo')); // e:\test\foo\boo
-// console.log(path.resolve('/test/a.js')); // e:\test\a.js
-// ..\..\a.js
-// console.log(path.resolve('/other/foo/boo')) // e:\other\foo
-// path.resolve('/test/a.js'); // e:\test\a.js
-// 没有公共路径。
-// let r = path.relative('/other/foo', '/test/a.js'); // ..\..\test\a.js
-// console.log(r);
-
-// ..\..\test\a.js
-// console.log(path.resolve(__dirname, '/test/a.js'));
-// ..\..\..\..\..\..\..\test\a.js
-// console.log(path.relative(__dirname, 'test/a.js'));
-// test\a.js
-
+// posix & win32
 console.log(path.posix.resolve('/test/foo'));
+console.log(path.win32.resolve('/test/foo'));
 console.log(path.resolve('/test/foo'));
 
+// base api
 console.log(path.basename('/test/foo', '.js'));
 console.log(path.basename('/test/foo.html', '.html'));
 
-console.log(path.win32.resolve('/test/foo'));
+console.log(process.env.path);
+
+console.log(path.join('/foo', 'bar', 'baz/asdf', 'quux', '..'));
+console.log(path.join('/foo', 'bar', 'baz/asdf', '/quux'));
+
+console.log(path.normalize('C:////temp\\\\/\\/\\/foo/..\\bar/test/hello.js'));
+console.log(path.parse('/foo/bar/test.js'));
+console.log(path.parse('C:\\foo\\bar\\test.js'));
